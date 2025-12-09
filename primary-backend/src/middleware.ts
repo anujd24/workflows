58c458ca8610ab9bd0136df4d767fc3344979f30
Anuj Dubey <anujd2408@gmail.com>
2025-12-09 17:32:07 +0530

    added the backend files

---
+++ primary-backend/src/middleware.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken"
+import { JWT_PASSWORD } from "./config";
+export function authMiddleware(req: Request, res: Response, next: NextFunction){
+    const token = req.headers.authorization as string | undefined;
+
+    if (!token) {
+        return res.status(403).json({
+            message : "You are not logged in"
+        });
+    }
+
+    try{
+        const payload = jwt.verify(token, JWT_PASSWORD);
+        //@ts-ignore
+        req.id = payload.id;
+        next();
+    }catch(e){
+        return res.status(403).json({
+            message : "You are not logged in"
+        })   
+    }
+}
