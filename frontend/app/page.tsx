6ae45303024b66fe3b87b93ed1255f6a831291aa
Anuj Dubey <anujd2408@gmail.com>
2026-02-02 13:13:39 +0530

    added

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,16 +1,21 @@
+"use client";
+
 import { Appbar } from "@/components/Appbar";
 import { Hero } from "@/components/Hero";
 import { HeroVideo } from "@/components/HeroVideo";
+import { Footer } from "@/components/Footer";
 
 export default function Home() {
   return (
-    <main className="pb-48">
-      <Appbar/>
-      <Hero/>
-      <div className="pt-8">
-        <HeroVideo/>
+    <main className="min-h-screen flex flex-col bg-white">
+      <Appbar />
+      <div className="flex-1">
+          <Hero />
+          <div className="pt-8 pb-20 px-4">
+            <HeroVideo />
+          </div>
       </div>
-      
+      <Footer />
     </main>
   );
 }
