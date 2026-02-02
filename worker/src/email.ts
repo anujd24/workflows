0767228a1345469e20c312f7326e375afa30e52a
Anuj Dubey <anujd2408@gmail.com>
2026-02-02 13:21:35 +0530

    added email file that sends an email to users

---
+++ worker/src/email.ts
@@ -0,0 +1,39 @@
+import nodemailer from "nodemailer";
+import {parse} from "./parser"
+
+
+export async function sendEmail(to: string, body: string, bindings: any) {
+    
+    console.log("metadata received:", JSON.stringify(bindings, null, 2));
+
+    const finalEmail = parse(to, bindings);
+    const finalBody = parse(body, bindings);
+
+    console.log(`parsing '${to}' and result is: '${finalEmail}'`);
+    
+    if (!finalEmail || finalEmail.includes("{") || finalEmail.trim() === "") {
+        return; 
+    }
+
+    const transport = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: 587,
+        secure: false, 
+        auth: {
+            user: process.env.SMTP_USERNAME,
+            pass: process.env.SMTP_PASSWORD,
+        },
+    });
+
+    try {
+        await transport.sendMail({
+            from: process.env.SMTP_USERNAME, 
+            to: finalEmail,
+            subject: "ZapFlux", 
+            text: finalBody,
+        });
+        console.log("email sent successfully to", finalEmail);
+    } catch (e) {
+        console.error(" nodemailer Error:", e);
+    }
+}
