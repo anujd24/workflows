5137278208849da40e2027f9f42b12af3eb2591b
Anuj Dubey <anujd2408@gmail.com>
2026-02-03 14:39:57 +0530

    added slash

--- primary-backend/src/index.ts
+++ primary-backend/src/index.ts
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("api/v1/user", userRouter);
+app.use("/api/v1/user", userRouter);
 
 app.use("/api/v1/zap", zapRouter);
 
