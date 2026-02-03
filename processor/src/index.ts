616fd8f5de1725a95c18bcfae9f3386b76e31bd7
Anuj Dubey <anujd2408@gmail.com>
2026-02-03 13:02:11 +0530

    added ca certificate as a variable

--- processor/src/index.ts
+++ processor/src/index.ts
@@ -1,37 +1,19 @@
 import { PrismaClient } from "@prisma/client";
 import { Kafka } from "kafkajs";
+import { Ca_Cert } from "./ca";
 import dotenv from "dotenv";
 dotenv.config();
 
 const TOPIC_NAME = "zap-events";
 const client = new PrismaClient;
 
-function getSSLConfig() {
-    const base64Cert = process.env.KAFKA_CA_CERT;
-    
-    if (base64Cert) {
-        console.log(" CA Certificate detected in Env Vars. Decoding...");
-        try {
-            const ca = Buffer.from(base64Cert, 'base64').toString('utf-8');
-            return {
-                ca: [ca],
-                rejectUnauthorized: true
-            };
-        } catch (e) {
-            console.error(" Failed to decode Certificate. Falling back to INSECURE mode.");
-        }
-    } else {
-        console.log(" No CA Certificate found. Using INSECURE mode (rejectUnauthorized: false).");
-    }
-    return {
-        rejectUnauthorized: false
-    };
-}
-
 const kafka = new Kafka({
     clientId : 'outbox-processor',
     brokers: [process.env.KAFKA_BROKER || "localhost:9092"], 
-    ssl: getSSLConfig(),
+    ssl: {
+        ca : [Ca_Cert],
+        rejectUnauthorized : true
+    },
     sasl: {
         mechanism: 'scram-sha-256', 
         username: process.env.KAFKA_USERNAME || "",
@@ -41,7 +23,14 @@ const kafka = new Kafka({
 
 async function main(){
     const producer = kafka.producer();
-    await producer.connect();
+    try{
+        await producer.connect();
+        console.log("kafka connected")
+    } catch(e){  
+        console.error("kafka connection failed : ", e)
+        return;
+    }
+    
 
     while(1){
         const pendingRows = await client.zapRunOutbox.findMany({
