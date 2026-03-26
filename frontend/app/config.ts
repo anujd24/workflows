47b2ccf52c93c38f18b3df0a6a8ce4c58cc7dcd1
Anuj Dubey <anujd2408@gmail.com>
2026-03-26 19:12:36 +0530

    changed url

--- frontend/app/config.ts
+++ frontend/app/config.ts
@@ -1,5 +1,11 @@
 
 
-export const BACKEND__URL = "https://zapflux-backend.onrender.com"
+// export const BACKEND__URL = "https://zapflux-backend.onrender.com" 
 
-export const HOOKS_URL = "https://zapflux-hooks.onrender.com"
+// export const HOOKS_URL = "https://zapflux-hooks.onrender.com"
+
+
+
+export const BACKEND__URL = "https://zapflux-primary-backend.onrender.com" 
+
+export const HOOKS_URL = "https://zapflux-hook.onrender.com"
