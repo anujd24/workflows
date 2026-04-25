e6bdb04443d3af4f4e98fc3126f3cdc63be36df0
Anuj Dubey <anujd2408@gmail.com>
2026-04-25 23:09:35 +0530

    fixed

--- processor/dist/index.js
+++ processor/dist/index.js
@@ -8,19 +8,50 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const client_1 = require("@prisma/client");
 const kafkajs_1 = require("kafkajs");
+const http_1 = __importDefault(require("http"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const server = http_1.default.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Processor is running fine!');
+});
+const port = process.env.PORT || 8080;
+server.listen(port, () => {
+    console.log(`server listening on port ${port}`);
+});
 const TOPIC_NAME = "zap-events";
 const client = new client_1.PrismaClient;
 const kafka = new kafkajs_1.Kafka({
     clientId: 'outbox-processor',
-    brokers: ['localhost:9092']
+    brokers: ["localhost:9092"],
+    // ssl: {
+    //     ca : [Ca_Cert.replace(/\\r\\n/g, "\\n")],
+    //     rejectUnauthorized : true,
+    //     checkServerIdentity : () => undefined
+    // },
+    // sasl: {
+    //     mechanism: 'scram-sha-256', 
+    //     username: process.env.KAFKA_USERNAME || "",
+    //     password: process.env.KAFKA_PASSWORD || "",
+    // }
 });
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
         const producer = kafka.producer();
-        yield producer.connect();
+        try {
+            yield producer.connect();
+            console.log("kafka connected");
+        }
+        catch (e) {
+            console.error("kafka connection failed : ", e);
+            process.exit(1);
+        }
         while (1) {
             const pendingRows = yield client.zapRunOutbox.findMany({
                 where: {},
@@ -52,7 +83,7 @@ function main() {
             }
             catch (error) {
                 console.error("Error sending batch to Kafka:", error);
-                yield new Promise(r => setTimeout(r, 1000));
+                yield new Promise(r => setTimeout(r, 2000));
             }
         }
     });
